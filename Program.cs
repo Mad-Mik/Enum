47d58ff8af4bea71c51f11e0c16f7cf53292b572
mikhail.madoyan <mikhail.madoyan@gmail.com>
2022-12-15 22:41:28 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Enum
+            DayTime dayTime = DayTime.Morning;
+
+            if (dayTime == DayTime.Morning)
+                Console.WriteLine("Good Morning");
+            else
+                Console.WriteLine("Hello");
+        }
+        enum DayTime
+        {
+            Morning,
+            Afternoon,
+            Evening,
+            Night
+        }
+    }
+}
